6b6816761fb9f85dcbb5b76ec46953ed22e05ab5
Thomas Grossen <github@grossen.me>
2026-05-03 07:31:01 +0200

    Initial Commit

---
+++ Reboxed/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Reboxed
+//
+//  Created by Thomas Grossen on 03.05.2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
